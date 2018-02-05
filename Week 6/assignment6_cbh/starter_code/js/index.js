226b4295dca042eee0fcf10cc2730eb51481c343
Caroline Heys <cbheys@gmail.com>
2018-02-05 18:37:25 -0500

    caroline

---
+++ Week 6/assignment6_cbh/starter_code/js/index.js
@@ -0,0 +1,11 @@
+//if NYC is selected, the background will turn to nyc.jpg
+//if SF is selected, the background will turn to sf.jpg
+//if LA is selected, the background will turn to  la.jpg
+//if ATX is selected, the background will turn to austin.jpg
+//if SYD is selected, the background will turn to sydney.jpg
+
+$(document).ready(function(){
+	alert('hello world!');
+
+	var city = ["NYC", "SF", "LA", "ATX", "SYD"];
+})
