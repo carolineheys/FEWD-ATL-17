b53ce1381e1c513e3c263b34602f6fb793506245
Caroline Heys <cbheys@gmail.com>
2018-02-06 19:27:03 -0500

    hw

--- Week 6/assignment6_cbh/starter_code/js/index.js
+++ Week 6/assignment6_cbh/starter_code/js/index.js
@@ -4,8 +4,27 @@
 //if ATX is selected, the background will turn to austin.jpg
 //if SYD is selected, the background will turn to sydney.jpg
 
-$(document).ready(function(){
-	alert('hello world!');
-
+$('#city-type').click(function(event) {
 	var city = ["NYC", "SF", "LA", "ATX", "SYD"];
-})
+	var text = "";
+	event.preventDefault();
+
+	for (i = 0; i < city.length; i++) { 
+		text += city[i] + "<br>";
+	}
+	document.getElementById("city-type").innerHTML = text;
+});
+
+function cityChosen(name) {
+	if (name == nyc) {
+		$('body').addClass('.nyc');
+	}else if(name == sf) {
+		$('body').addClass('.sf');
+	}else if(name == la) {
+		$('body').addClass('.la');
+	}else if(name == atx) {
+		$('body').addClass('.austin');
+	}else {
+		$('body').addClass('.sydney');
+	}
+}
