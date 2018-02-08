cfe71439434b4c8e62eab249a798d1fe058f96fe
Caroline Heys <cbheys@gmail.com>
2018-02-07 19:59:21 -0500

    homework

--- Week 6/assignment6_cbh/starter_code/js/index.js
+++ Week 6/assignment6_cbh/starter_code/js/index.js
@@ -4,27 +4,11 @@
 //if ATX is selected, the background will turn to austin.jpg
 //if SYD is selected, the background will turn to sydney.jpg
 
-$('#city-type').click(function(event) {
-	var city = ["NYC", "SF", "LA", "ATX", "SYD"];
-	var text = "";
-	event.preventDefault();
+	var cityArr = ["NYC", "SF", "LA", "ATX", "SYD"];
 
-	for (i = 0; i < city.length; i++) { 
-		text += city[i] + "<br>";
-	}
-	document.getElementById("city-type").innerHTML = text;
-});
+	$.each(cityArr, function(index, value){
+		//console.log(value);
 
-function cityChosen(name) {
-	if (name == nyc) {
-		$('body').addClass('.nyc');
-	}else if(name == sf) {
-		$('body').addClass('.sf');
-	}else if(name == la) {
-		$('body').addClass('.la');
-	}else if(name == atx) {
-		$('body').addClass('.austin');
-	}else {
-		$('body').addClass('.sydney');
-	}
-}
+		$('select').append('<option>'+ value +'</option>')
+
+	});
